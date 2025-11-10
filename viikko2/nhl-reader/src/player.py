4f60bb0f445f20278d4ba9be90846830fddc69db
Ricky <rrautiainen13@gmail.com>
2025-11-10 22:21:35 +0200

    added nhl-reader project for Finnish players

---
+++ viikko2/nhl-reader/src/player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self, dict):
+        self.name = dict['name']
+        self.team = dict['team']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
+        self.nationality = dict['nationality']
+    
+    def __str__(self):
+        return f"{self.name:20} team {self.team:3}  goals {self.goals:2} assists {self.assists:2}"
